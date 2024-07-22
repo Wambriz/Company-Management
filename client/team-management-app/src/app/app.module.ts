851cb4d65815f36dab0429bb570c088c26e3d336
Josue Jovel <josuejovel1234@gmail.com>
2024-07-22 14:50:31 -0500

    Infrastructure for card for creating a new team

--- client/team-management-app/src/app/app.module.ts
+++ client/team-management-app/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TeamsComponent } from './teams/teams.component';
 import { TeamCardComponent } from './teams/team-card/team-card.component';
+import { CreateTeamCardComponent } from './teams/create-team-card/create-team-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TeamsComponent,
-    TeamCardComponent
+    TeamCardComponent,
+    CreateTeamCardComponent
   ],
   imports: [
     BrowserModule,
---
+++ client/team-management-app/src/app/teams/create-team-card/create-team-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-team-card',
+  templateUrl: './create-team-card.component.html',
+  styleUrl: './create-team-card.component.css'
+})
+export class CreateTeamCardComponent {
+
+}
