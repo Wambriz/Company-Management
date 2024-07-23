b6bed42250af6dc00ded670af4ec0214c9692e2a
LarryDevinCarter <larrydevincarter@gmail.com>
2024-07-22 19:43:49 -0500

    Added logic for Select Company dropdown, basic HTLM

--- client/team-management-app/src/app/select-company/select-company.component.ts
+++ client/team-management-app/src/app/select-company/select-company.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-select-company',
   templateUrl: './select-company.component.html',
-  styleUrl: './select-company.component.css'
+  styleUrls: ['./select-company.component.css'],
 })
 export class SelectCompanyComponent {
+  selectedCompany: string | null = null;
+  companies = ['FedEx', 'Cook Systems', 'Google'];
 
+  constructor(private router: Router) {}
+
+  onSelectCompany(event: Event): void {
+    const selectElement = event.target as HTMLSelectElement;
+    this.selectedCompany = selectElement.value;
+    if (this.selectedCompany) {
+      localStorage.setItem('selectedCompany', this.selectedCompany); //save company in local storage & and goto home/announcemnets page -Devin
+      this.router.navigate(['/home-announcements']); 
+    }
+  }
 }
