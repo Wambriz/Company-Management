cc4a5882712045db44143462216ea009464c7fe5
William Ambriz <Wambriz@me.com>
2024-07-23 23:01:31 -0700

    Setup files

--- server/Management-App/src/main/java/com/teamblue/Management_App/controllers/CompanyController.java
+++ server/Management-App/src/main/java/com/teamblue/Management_App/controllers/CompanyController.java
@@ -2,17 +2,16 @@ package com.teamblue.Management_App.controllers;
 
 import java.util.List;
 
+import com.teamblue.Management_App.services.CompanyService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
-@RequestMapping("")
+@RequestMapping("/company")
 public class CompanyController {
+    @Autowired
+    private CompanyService companyService;
 
-//    @Autowired
-//    private CompanyService companyService;
-
+    // TODO: ENDPOINTS
 }
